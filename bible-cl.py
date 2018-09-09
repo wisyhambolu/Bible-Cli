100c934b06d252ebcbf07746864d66dfc6db7466
Wisdom Hambolu <wisyhambolu@gmail.com>
2018-09-09 10:41:20 +0100

    Fixed input format with argparse function

--- bible-cl.py
+++ bible-cl.py
@@ -1,12 +1,51 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+#import modules
 import json
 import sys
+import os
+import argparse
+import re
 from difflib import get_close_matches
+from prettytable import PrettyTable, ALL
 
+parser = argparse.ArgumentParser()
+
+#usage statement
+parser.add_argument("scripture",help="'1 John 1:2-5'")
+
+#load json dataset
 dataset = json.load(open("dataset.json"))
 
+#create PrettyTable object; set Header to True; hrules sets borders between rows
+table = PrettyTable(["Scripture","Text"],hrules=ALL,header=True)
+
+#get the length of th terminal and resize text
+terminal_length = os.get_terminal_size()[0] - os.get_terminal_size()[0] / 3
+
+#function is format scripture text
+def format_text(text,max_length):
+    #accumulated line length
+    ACC_length = 0
+    words = text.split(" ")
+    formatted_text = ""
+    for word in words:
+        #if ACC_length + len(word) and a space is <= max_line_length 
+        if ACC_length + (len(word) + 1) <= max_length:
+            #append the word and a space
+            formatted_text = formatted_text + word + " "
+            #length = length + length of word + length of space
+            ACC_length = ACC_length + len(word) + 1
+        else:
+            #append a line break, then the word and a space
+            formatted_text = formatted_text + "\n" + word + " "
+            #reset counter of length to the length of a word and a space
+            ACC_length = len(word) + 1
+    return formatted_text
+
+#Function to lookup scripture
 def lookup(book,chapter,verse,end_verse):
 
+    #try-except statements to catch ValueErrors, -1 due to zero index
     try:
         chapter=int(chapter) - 1
     except ValueError:
@@ -22,88 +61,138 @@ def lookup(book,chapter,verse,end_verse):
     except ValueError:
         return "Error : The End Verse is expected to be a digit, Please try again"
 
+    #loop json dataset; use 999998 as empty parameter indicator
     for dataset_item in dataset:
         if book.lower() in dataset_item["name"].lower():
             if chapter == 999998:
-                return { 'book' : dataset_item["chapters"] }
+                    return { 'book' : dataset_item["chapters"] }
             elif verse == 999998:
-                return dataset_item["chapters"][chapter]
+                try:
+                    return dataset_item["chapters"][chapter]
+                except IndexError:
+                    return "Error: Chapter incorrect, Please check and try again"
             elif end_verse != 999998:
                 end_verse=(end_verse+1)
-                return tuple( dataset_item["chapters"][chapter][verse:end_verse] )
+                try:
+                    return tuple( dataset_item["chapters"][chapter][verse:end_verse] )
+                except IndexError:
+                    return "Error: Start/End Verse Incorrect, Please check and try again"
             else:
-                return dataset_item["chapters"][chapter][verse]
-                
-param1 = all
-if sys.argv[1:]:
-    param1=sys.argv[1]
+                try:
+                    return dataset_item["chapters"][chapter][verse]
+                except IndexError:
+                    return "Error : Verse is incorrect, Please check and try again"
 
-param2 = 999999
-if sys.argv[2:]:
-    param2=sys.argv[2]
+#read command line arguments
+args = parser.parse_args()
 
-param3 = 999999
-if sys.argv[3:]:
-    if sys.argv[3] == ":":
-        if sys.argv[4:]:
-            param3 = sys.argv[4]
+#check if the inputted string can be split
+scripture_string = re.split('[:-]',args.scripture)
+
+
+if len(scripture_string) > 1: #eg: 1 John 2:1 [ '1 John 2', '1' ]
+    book = scripture_string[:1] #get only first item in list
+    verse_start_end = scripture_string[1:] #get only last item in list
+
+    if len(verse_start_end) == 2:   
+        start_verse = verse_start_end[0]
+        end_verse = verse_start_end[1]
     else:
-        param3=sys.argv[3]
+        start_verse = verse_start_end[0]
+    
+    book = ' '.join(scripture_string[0].split()[:-1]) #convert list to string
+    chapter = ' '.join(scripture_string[0].split()[-1]) #convert list to string
 
-param4 = 999999
-if sys.argv[5:]:
-    if sys.argv[5] == "-":
-        if sys.argv[6:]:
-            param4 = sys.argv[6]
-            
-output=(lookup(param1,param2,param3,param4))
+else: #eg: 1 John 2 ['1 John 2']
+    if scripture_string[0].split()[-1].isdigit() == True: #check if book has a chapter
+        book = ' '.join(scripture_string[0].split()[:-1]) #convert list to string
+        chapter = ' '.join(scripture_string[0].split()[-1]) #convert list to string
+    else:
+        book = ' '.join(scripture_string) #convert list to string
+
+
+#try except blocks to catch undefined variables
+try:
+    book
+except NameError:
+    book = ""
+
+try:
+    chapter
+except NameError:
+    chapter = ""
+
+try:
+    start_verse
+except NameError:
+    start_verse = ""    
+
+try:
+    end_verse
+except NameError:
+    end_verse = ""
+
+
+#check if variables are empty string and set default value
+book_input = all
+if book != "":
+    book_input = book
+
+chapter_input = 999999
+if chapter != "":
+    chapter_input = chapter
+
+start_verse_input = 999999
+if start_verse != "":
+    start_verse_input = start_verse
+
+end_verse_input = 999999
+if end_verse != "":
+    end_verse_input = end_verse
+
+##""" Possible inputs:
+#1 kings
+#1 kings 1
+#1 kings 1-2
+#1 kings 1:1
+#1 kings 1:1-2
+#Song of Solomon 1
+#John 1 """
+
+#lookup scripture with function and parameters            
+output=(lookup(book_input,chapter_input,start_verse_input,end_verse_input))
+
+#different input scenarios are differentiated by different data types
+#dict ===> book only
+#list ===> book and chapter
+#str ===> book, chapter and start verse
+#tuple ===> book, chapter, start verse and end verse
 if type(output) == dict:
-    output_len=len(output["book"]) 
+    #book is the key in the dictionary "output"
+    output_len=len(output["book"]) #get length of dictionary, number of chapters
     output=output["book"]
 
-    for item in range(output_len):
-        #if type(item) == list:
-        for sub_item,sub_item_count in zip(output[item],range(len(output[item]))):
-            print(148*"-")
-            print(param1 + " " + str(item + 1) + " : " + str(sub_item_count + 1) + " | " + sub_item)
-            print(148*"-")
+    for chapter in range(output_len):
+        for text,verse in zip(output[chapter],range(len(output[chapter]))):
+            text=format_text(text,terminal_length)
+            table.add_row([book_input + " " + str(chapter + 1) + " : " + str(verse + 1),text])  #add row to output table
+    print(table)
+
 elif type(output) == list:
     output_len=len(output)
-    for item,item_count in zip(output,range(len(output))):
-            print(148*"-")
-            print(param1 + " " + param2 + " : " + str(item_count + 1) + " | " + item)
-            print(148*"-")
-elif type(output) == unicode:
-    print(148*"-")
-    print(param1 + " " + param2 + " : " + param3 + " | " + output)
-    print(148*"-")
-elif type(output) == tuple:
-    param4=(int(param4)+1)
-    for item,item_count in zip(output,range(int(param3),int(param4))):
-            print(148*"-")
-            print(param1 + " " + param2 + " : " + str(item_count) + " | " + item)
-            print(148*"-")
+    for text,verse in zip(output,range(len(output))):
+        text=format_text(text,terminal_length)
+        table.add_row([book_input + " " + chapter_input + " : " + str(verse + 1),text]) #add row to output table
+    print(table)
+
 elif type(output) == str:
-    print(output)
+    text=format_text(output,terminal_length)
+    table.add_row([book_input + " " + chapter_input + " : " + start_verse_input,text])  #add row to output table
+    print(table)
 
-
-#def search(book):
-#    book = book.lower()
-#    if book in dataset:
-#        return dataset[book]
-    #elif len(get_close_matches(word, dataset.keys())[0]) > 0:
-    #    ans = input("Did you mean \"%s\" instead? Y or N: " % get_close_matches(word, dataset.keys())[0])
-    #    if ans == "Y":
-    #        return dataset[get_close_matches(word, dataset.keys())[0]]
-    #    elif ans == "N":
-    #        return "The word \""+ word +"\" doesn't exist, please double check and try again"
-#    else:
-#        return "The word \""+ book +"\" doesn't exist, please double check and try again"
-
-#word = input("Enter a book: ")
-#output=(search(book))
-#if type(output) == list:
-#    for item in output:
-#        print(item)
-#else:
-#    print(output)
+elif type(output) == tuple:
+    end_verse_input=(int(end_verse_input)+1)
+    for text,start_verse in zip(output,range(int(start_verse_input),int(end_verse_input))):
+            text=format_text(text,terminal_length)
+            table.add_row([book_input + " " + chapter_input + " : " + str(start_verse),text])   #add row to output table          
+    print(table)
